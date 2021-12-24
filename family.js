2d1d8a33cbb978d5eee9fc10dcdf8a95b347f5d5
freelancerakash07 <freelancerakash07@gmail.com>
2021-12-24 21:58:25 +0600

    upload all fiile

---
+++ family.js
@@ -0,0 +1,84 @@
+const family = [
+    
+    {
+      id       : 1,
+      name     : 'Akash',
+      phone    : '01324516475',
+      age      : 25,
+      location : 'Dhaka',
+      photo    : 'https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg'
+
+    },
+    {
+      id       : 2,
+      name     : 'Korim',
+      phone    : '01324516475',
+      age      : 20,
+      location : 'Dinajpur',
+      photo    : 'https://thumbs.dreamstime.com/b/portrait-attractive-cheerful-young-man-smiling-happy-face-human-expressions-emotions-model-beautiful-smile-blue-eyes-147751681.jpg'
+    },
+    {
+      id       : 3,
+      name     : 'Achem',
+      phone    : '01324516475',
+      age      : 21,
+      location : 'Dinajpur',
+      photo    : 'https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg'
+    },
+    {
+      id       : 4,
+      name     : 'Arman',
+      phone    : '01324516475',
+      age      : 22,
+      location : 'Rajshahi',
+      photo    : 'https://img.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-23637.jpg?size=626&ext=jpg'
+    },
+    {
+      id       : 5,
+      name     : 'Salman',
+      phone    : '01324516475',
+      age      : 24,
+      location : 'Jamalpur',
+      photo    : 'https://image.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg'
+    },
+    {
+      id       : 6,
+      name     : 'Arif',
+      phone    : '01324516475',
+      age      : 25,
+      location : 'Rangpur',
+      photo    : 'https://static1.bigstockphoto.com/5/0/3/large1500/305931643.jpg'
+    },
+    {
+      id       : 7,
+      name     : 'Kamal',
+      phone    : '01324516475',
+      age      : 26,
+      location : 'Chainobajong',
+      photo    : 'https://thumbs.dreamstime.com/b/head-shot-normal-face-man-asian-103009511.jpg'
+    },
+    {
+      id       : 8,
+      name     : 'Omol',
+      phone    : '01324516475',
+      age      : 27,
+      location : 'Bogura',
+      photo    : 'https://www.maxpixel.net/static/photo/1x/Person-Male-Face-Portrait-Human-Teenage-Man-Boy-6040022.jpg'
+    },
+    {
+      id       : 9,
+      name     : 'Komol',
+      phone    : '01324516475',
+      age      : 28,
+      location : 'Ghulsan',
+      photo    : 'https://www.uni-regensburg.de/Fakultaeten/phil_Fak_II/Psychologie/Psy_II/beautycheck/english/prototypen/m_sexy_gr.jpg'
+    },
+    {
+      id       : 10,
+      name     : 'Korim',
+      phone    : '01324516475',
+      age      : 29,
+      location : 'Syllet',
+      photo    : 'https://i.pinimg.com/originals/c7/d7/38/c7d738b705c2ea2648fb7a09396ea0c5.jpg'
+    },
+];
---
+++ student.js
@@ -0,0 +1,621 @@
+const studentlist = [
+
+    {
+       id       : 1,
+       name     : 'Md Kayda Azam',
+       roll     : 145687,
+       reg      : 45879658423,
+       age      : 30,
+       fname    : 'Asraf',
+       mname    : 'Rohima',
+       location : 'Banani',
+       gender   : 'Male',
+       photo    : 'https://image.shutterstock.com/image-photo/close-photo-charming-guy-entrepreneur-260nw-1531460651.jpg'
+   },
+
+    {
+       id       : 2,
+       name     : 'sadiq',
+       roll     : 145687,
+       reg      : 45879658423,
+       age      : 25,
+       fname    : 'Asraf',
+       mname    : 'Rohima',
+       location : 'Mirpur',
+       gender   : 'Male',
+       photo    : 'https://image.shutterstock.com/image-photo/cheerful-positive-guy-beard-rejoicing-260nw-1478799818.jpg'
+   },
+   {
+    id       : 3,
+    name     : 'Ava',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 30,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Female',
+    photo    : 'https://media.istockphoto.com/photos/portrait-of-a-middle-eastern-beautiful-woman-picture-id147444440?k=20&m=147444440&s=612x612&w=0&h=jxAMJdR3NhkkrJmEWuAXvOi0OJg-JMFqNuuH1UhU2aA='
+   },
+   
+{
+    id       : 4,
+    name     : 'Korim',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 23,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Dhaka',
+    gender   : 'Male',
+    photo    : 'https://akm-img-a-in.tosshub.com/indiatoday/images/story/202106/Gray_Man.jpg?TIM.pBR2amOelRvfmi4Xnqd2cOqL83JW&size=770:433'
+},
+
+ {
+    id       : 5,
+    name     : 'Sophia',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 22,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Female',
+    photo    : 'https://img.freepik.com/free-photo/muslim-woman-niqab-niqab-saudi_230311-30098.jpg?size=338&ext=jpg'
+},
+
+ {
+    id       : 6,
+    name     : 'Jobbar',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 21,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Dhaka',
+    gender   : 'Male',
+    photo    : 'https://steezo.com/wp-content/uploads/2012/12/man-in-suit2.jpg'
+},
+
+ {
+    id       : 7,
+    name     : 'Amelia',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 20,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Female',
+    photo    :  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2kzx4VgTsHQX0-JNq35bjo5qdhTfZ8eWHRJU4T9sM_S8er5sXJ-87fKoTFWFxBUpKVC8&usqp=CAU'
+},
+
+ {
+    id       : 8,
+    name     : 'Abida Jahan',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Dhaka',
+    gender   : 'Female',
+    photo    : 'https://i2-prod.dailystar.co.uk/incoming/article19715152.ece/ALTERNATES/s615b/Burqa-Syria-429603'
+},
+ {
+    id       : 9,
+    name     : 'Isabella',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Gazipur',
+    gender   : 'Female',
+    photo    : 'https://media.istockphoto.com/photos/woman-wearing-hijab-picture-id75407736?k=20&m=75407736&s=612x612&w=0&h=yQAiT4Uhj9O52dCsQ78I7J_sK6akXT3Z115tSfa4Rc8='
+},
+ {
+    id       : 10,
+    name     : 'Miana',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Female',
+    photo    : 'https://en.islami.co/wp-content/uploads/2020/02/niqab.jpg'
+},
+ {
+    id       : 11,
+    name     : 'Alexander',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Rangpur',
+    gender   : 'Male',
+    photo    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjRSuSfyqoAnYJsViQClfkk03AwQwRfONHjeO6yhRnp2_FeOQwIycA9d7zFxJH2Pv9yCs&usqp=CAU'
+},
+ {
+    id       : 12,
+    name     : 'Evelyn',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Female',
+    photo    : 'https://smartcdn.prod.postmedia.digital/torontosun/wp-content/uploads/2017/10/1020_na_niqab_59820910-e1508777811806.jpg?quality=100&strip=all'
+},
+ {
+    id       : 13,
+    name     : 'Mason',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Male',
+    photo    : 'https://cdn.shopify.com/s/files/1/0045/5104/9304/products/forming-cream-boost-powder-model-2020_1024x1024.jpg?v=1599603786'
+},
+ {
+    id       : 14,
+    name     : 'Harper',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Female',
+    photo    : 'https://i.pinimg.com/originals/73/5e/6b/735e6bb37fc7fc8a1ddcb03800457a7a.jpg'
+},
+ {
+    id       : 8,
+    name     : 'Michael',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Rangpur',
+    gender   : 'Male',
+    photo    : 'https://cdn.shopify.com/s/files/1/0045/5104/9304/products/firm-hold-gel-model-2020_1024x1024.jpg?v=1631288809'
+},
+ {
+    id       : 15,
+    name     : 'Camila',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Female',
+    photo    : 'http://i.dailymail.co.uk/i/pix/2010/04/08/article-1264535-023C9059000005DC-950_233x369.jpg'
+},
+ {
+    id       : 16,
+    name     : 'Ethan',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Rangpur',
+    gender   : 'Male',
+    photo    : 'https://i.pinimg.com/236x/a7/c7/e1/a7c7e15d8510babe62bfecac15c73f3b--arab-men-facial-hair.jpg'
+},
+ {
+    id       : 17,
+    name     : 'Gianna',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Female',
+    photo    : 'https://static.india.com/wp-content/uploads/2016/10/1167917-burqa-14718945251-414x246.jpg'
+},
+ {
+    id       : 18,
+    name     : 'Daniel',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Male',
+    photo    : 'https://i.pinimg.com/474x/f8/0e/0c/f80e0cea7d74a2951468d59261f81e2a--middle-eastern-men-arab-swag.jpg'
+},
+ {
+    id       : 19,
+    name     : 'Abigail',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Female',
+    photo    : 'https://t3.ftcdn.net/jpg/02/91/34/60/360_F_291346015_V3vy7Y3myvHM7LKhU7hhROrcosoPEb3a.jpg'
+},
+ {
+    id       : 20,
+    name     : 'Jacob',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur ',
+    gender   : 'Male',
+    photo    : 'https://images.pexels.com/videos/9117001/pexels-photo-9117001.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+},
+ {
+    id       : 21,
+    name     : 'Luna',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Female',
+    photo    : 'https://i2.wp.com/arabicmehndidesign.com/wp-content/uploads/2020/11/Download-100-Latest-Islamic-Dp-For-Girls-Best-Pics-6.png?fit=518%2C561&ssl=1'
+},
+ {
+    id       : 22,
+    name     : 'Logan',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Male',
+    photo    : 'https://image.shutterstock.com/image-photo/arabic-little-boy-doing-homework-260nw-290124329.jpg'
+},
+ {
+    id       : 23,
+    name     : 'Ella',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Rangpur',
+    gender   : 'Female',
+    photo    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqqWLdD9yfXoO3LBXDdahXuN_YSZGVTjYeTA&usqp=CAU'
+},
+ {
+    id       : 24,
+    name     : 'Jamal',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Male',
+    photo    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQImxMYMfFiKZK_dN8dKJsCmE3wQ0bq0dS4A&usqp=CAU'
+},
+ {
+    id       : 25,
+    name     : 'Sofia',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Mirpur',
+    gender   : 'Female',
+    photo    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn8jRvrzIvs3JxrpQXHD5URI-L4_Zdk8ABaA&usqp=CAU'
+},
+  {
+    id       : 26,
+    name     : 'Hasan',
+    roll     : 145687,
+    reg      : 45879658423,
+    age      : 18,
+    fname    : 'Asraf',
+    mname    : 'Rohima',
+    location : 'Banani',
+    gender   : 'Male',
+    photo    : 'https://media-exp1.licdn.com/dms/image/C5603AQGi9UsmxbS7lg/profile-displayphoto-shrink_100_100/0/1635825895046?e=1643241600&v=beta&t=7pSdE8wAsvh9rJiv-Nod44nY2tFL7WtBOjP_mQ7cnG4'
+},
+    
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  {
+//     id       : 27,
+//     name     : 'Elizabeth',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 28,
+//     name     : 'Mijan',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 29,
+//     name     : 'sathi',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Mirpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 30,
+//     name     : 'Qader',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 31,
+//     name     : 'sonia',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 32,
+//     name     : 'Ashik',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 33,
+//     name     : 'Lamia',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Mirpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 34,
+//     name     : 'Rifat',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 35,
+//     name     : 'Afrin',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Mirpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 36,
+//     name     : 'Milon',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 37,
+//     name     : 'Emily',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Mirpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 38,
+//     name     : 'Alamin',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 39,
+//     name     : 'Dilara',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 40,
+//     name     : 'Henry',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 41,
+//     name     : 'Lucas',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 42,
+//     name     : 'Emma',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 43,
+//     name     : 'Benjamin',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 44,
+//     name     : 'Olivia',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'female'
+// },
+//  {
+//     id       : 45,
+//     name     : 'AJames',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Cumilla',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 46,
+//     name     : 'William',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Uttora',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 47,
+//     name     : 'Elijah',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Syllet',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 48,
+//     name     : 'Oliver',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Banani',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 49,
+//     name     : 'Noah',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'male'
+// },
+//  {
+//     id       : 50,
+//     name     : 'Liam',
+//     roll     : 145687,
+//     reg      : 45879658423,
+//     age      : 18,
+//     fname    : 'Asraf',
+//     mname    : 'Rohima',
+//     location : 'Rangpur',
+//     gender   : 'male'
+// },
+
